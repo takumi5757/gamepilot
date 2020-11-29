97b3bfffe98da27a315fb52e296d1012884418f6
saito <takumi5757@gmail.com>
2020-11-29 20:50:43 +0900

    速度認識 acc:0.474→0.910

--- GetSpeedValue/get_speed_value.py
+++ GetSpeedValue/get_speed_value.py
@@ -3,30 +3,51 @@ from PIL import Image
 import sys
 import pyocr
 import pyocr.builders
+import argparse
+import xgboost as xgb
+import numpy as np
+import cv2
 
 
-def get_speed_value(tool):
+def get_speed_value(mode="xgb"):
     """ディスプレイ左上に置いた1280*720のeurotruck2の画面から、speedを読み取る
     Args:
         tool(module):OCR(tesseract)
     """
-    bbox = (1000, 510, 1020, 530)
+    bbox = (1000, 510, 1016, 526)
 
-    while True:
-        try:
-            temp_pil_im = ImageGrab.grab(bbox=bbox)
+    if mode == "ocr":
+        tool = setup_ocr()
+        while True:
+            try:
+                temp_pil_im = ImageGrab.grab(bbox=bbox)
 
-            txt = tool.image_to_string(
-                temp_pil_im,
-                lang="eng",
-                builder=pyocr.builders.DigitBuilder(tesseract_layout=6),
-            )
-            if txt.isdigit():
-                print(f"speed:{int(txt)}")
+                speed_value = tool.image_to_string(
+                    temp_pil_im,
+                    lang="eng",
+                    builder=pyocr.builders.DigitBuilder(tesseract_layout=6),
+                )
+                if txt.isdigit():
+                    print(f"speed:{int(speed_value)}")
 
-        except KeyboardInterrupt:
-            print("end")
-            break
+            except KeyboardInterrupt:
+                print("end")
+                break
+
+    if mode == "xgb":
+        bst = xgb.Booster(model_file="models/xgbClassifier.json")
+
+        while True:
+            try:
+                temp_pil_im = ImageGrab.grab(bbox=bbox)
+                im_normalized = np.asarray(temp_pil_im) / 255.0
+                speed_value = bst.predict(xgb.DMatrix(im_normalized.reshape(1, 768)))
+
+                print(f"speed:{int(speed_value)}")
+
+            except KeyboardInterrupt:
+                print("end")
+                break
 
 
 def cv2pil(image):
@@ -37,7 +58,6 @@ def cv2pil(image):
     Return:
         new_image(PIL.Image.Image):PIL型の画像データ
     """
-    import cv2
 
     new_image = image.copy()
     if new_image.ndim == 2:  # モノクロ
@@ -47,6 +67,18 @@ def cv2pil(image):
     elif new_image.shape[2] == 4:  # 透過
         new_image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGBA)
     new_image = Image.fromarray(new_image)
+    return new_image
+
+
+def pil2cv(image):
+    """ PIL型 -> OpenCV型 """
+    new_image = np.array(image, dtype=np.uint8)
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
     return new_image
 
 
@@ -73,5 +105,11 @@ def setup_ocr():
 
 
 if __name__ == "__main__":
-    ocr_tool = setup_ocr()
-    get_speed_value(ocr_tool)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ocr", action="store_true", help="use ocr")
+    args = parser.parse_args()
+
+    if args.ocr:
+        get_speed_value(mode="ocr")
+    else:
+        get_speed_value(mode="xgb")
